1ac85cb60a31950646aecb8f06aae83dcc2abe41
lucasg18c <lucasg18c@gmail.com>
2021-05-02 16:47:11 -0300

    arreglos iniciales

--- Program.cs
+++ Program.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using SimulacionTP4.Presentacion;
+using System;
 using System.Windows.Forms;
 
 namespace SimulacionTP4
